97f36387fa967ee48feb6a395c1baca967d6fba9
Bharat Sharma <bharat.sharma1809@gmail.com>
2020-05-08 00:19:36 +0530

    Initialized splash screen

---
+++ unit_converter_app/lib/screens/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flare_dart/actor.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
