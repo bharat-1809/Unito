c48aed631b8f0846320232b11a214c6aa986645c
Bharat Sharma <bharat.sharma1809@gmail.com>
2020-05-08 01:07:37 +0530

    Added final splash screen

--- unit_converter_app/lib/screens/splash_screen.dart
+++ unit_converter_app/lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:flare_dart/actor.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -7,8 +9,41 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = Duration(seconds: 2);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/CategoryScreen');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _height = MediaQuery.of(context).size.height;
+    final _width = MediaQuery.of(context).size.width;
+    return Container(
+      width: _width,
+      height: _height,
+      color: Colors.white,
+      child: Center(
+        child: Container(
+          width: _height / 2,
+          height: _height / 2,
+          child: FlareActor(
+            'assets/flare/splash.flr',
+            alignment: Alignment.center,
+            animation: 'jump',
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
   }
 }
