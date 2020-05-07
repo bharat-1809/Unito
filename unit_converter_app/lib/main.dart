03e1a926bee56a346a17a55e68c31afead3bc955
Bharat Sharma <bharat.sharma1809@gmail.com>
2020-05-08 01:32:57 +0530

    Prventing device orientation

--- unit_converter_app/lib/main.dart
+++ unit_converter_app/lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:unitconverterapp/app.dart';
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
 import 'package:unitconverterapp/theme/themeChanger.dart';
 
-void main() => runApp(
-      UnitConverterApp(),
-      // DevicePreview(
-      //   builder: (context) => UnitConverterApp(),
-      // ),
-    );
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) => runApp(
+            UnitConverterApp(),
+            // DevicePreview(
+            //   builder: (context) => UnitConverterApp(),
+            // ),
+          ));
+}
 
 /// Unit Converter App.
 /// Main Parent Widget
